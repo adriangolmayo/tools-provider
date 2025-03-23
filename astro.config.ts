03006dc638468a066dbbc21ddddb5e69472437ac
al394753 <al394753@uji.es>
2025-03-23 18:03:11 +0100

    Remove duplicated workflow and remove not found function

--- astro.config.ts
+++ astro.config.ts
@@ -5,6 +5,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     output: 'static', 
-    adapter: github(),
     base: 'tools-provider', 
 });
