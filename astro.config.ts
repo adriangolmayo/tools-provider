5a3fd9e703e8736629581f8e3e12300a5db99fbb
al394753 <al394753@uji.es>
2025-08-07 23:07:12 +0200

    Change url

--- astro.config.ts
+++ astro.config.ts
@@ -5,5 +5,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://adriangolmayo.github.io',
-    base: '/tools-provider/', 
+    base: 'tools-provider', 
 });
