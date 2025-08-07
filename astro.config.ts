41b67f8c3ea8656b2c843a43157617dfc76c9d4e
al394753 <al394753@uji.es>
2025-08-07 22:58:10 +0200

    Remove output

--- astro.config.ts
+++ astro.config.ts
@@ -5,6 +5,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://adriangolmayo.github.io',
-    output: 'static', 
     base: '/tools-provider/', 
 });
