6058ada61d0791aa704854c4e653096dc6987af3
al394753 <al394753@uji.es>
2025-08-07 22:50:24 +0200

    Add site in astro config

--- astro.config.ts
+++ astro.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://adriangolmayo.github.io',
     output: 'static', 
-    base: 'tools-provider', 
+    base: '/tools-provider', 
 });
